cdba778e0e184b7d7a2e8529406effe8c35b6ff8
Daniel Otieno <danielotieno.ke@gmail.com>
2023-11-07 13:06:21 +0300

    add github action

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('cypress');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  projectId: 'szt5kr',
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  watchForFileChanges: false,
+  defaultCommandTimeout: 20000,
+  chromeWebSecurity: false,
+  requestTimeout: 30000,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
@@ -16,5 +23,7 @@ module.exports = defineConfig({
       allureWriter(on, config);
       return config;
     },
+    baseUrl: 'https://app.storyblok.com/#/login',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 });
